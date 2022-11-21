2b7d8348d4ffbfe2c7df39c60342f0d32bad87fc
Benjamin <bstanf@uw.edu>
2022-11-21 15:19:02 -0800

    rebuilt app.  Now funcitonal

---
+++ my-portfolio/src/components/MainContainer.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+// import About from './pages/About';
+// import Portfolio from './pages/Portfolio';
+// import Contact from './pages/Contact';
+// import Resume from './pages/Resume';
+import Footer from './Footer';
+import Header from './Header';
+import { Container } from '@mui/material';
+
+export default function MainContainer() {
+    <Container>
+        <Header />
+        <Footer />
+    </Container>
+}
